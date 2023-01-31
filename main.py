4e429aa8d8ad8f0225e0f992c932856ae8e0c12c
aleksypazuraPrivate <aleksy.pazura@onet.pl>
2023-01-31 20:17:48 +0100

    neural network - siec rozpoznaje juz zwierze

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
 import cv2
 import numpy as np
 import os
 from random import shuffle
 from tqdm import tqdm
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 # HomePC
-#TRAIN_DIR = 'C:/Users/aleks/PycharmProjects/pythonProject/siec_neuronowa_projekt/train'
-#TEST_DIR = 'C:/Users/aleks/PycharmProjects/pythonProject/siec_neuronowa_projekt/test'
+TRAIN_DIR = 'C:/Users/aleks/PycharmProjects/pythonProject/siec_neuronowa_projekt/train'
+TEST_DIR = 'C:/Users/aleks/PycharmProjects/pythonProject/siec_neuronowa_projekt/test'
 
 # Laptop
-TRAIN_DIR = 'C:/Users/aleks/PycharmProjects/siec_neuronowa_projekt/train'
-TEST_DIR = 'C:/Users/aleks/PycharmProjects/siec_neuronowa_projekt/test'
+# TRAIN_DIR = 'C:/Users/aleks/PycharmProjects/siec_neuronowa_projekt/train'
+# TEST_DIR = 'C:/Users/aleks/PycharmProjects/siec_neuronowa_projekt/test'
 IMG_SIZE = 50
 LR = 1e-3
 
-MODEL_NAME = 'sc_projekt-{}-{}.model'.format(LR, '2conv-basic-video')
+MODEL_NAME = 'sc_projekt-{}-{}.model'.format(LR, '6conv-basic-video')
 
 
 def label_img(img):
@@ -46,9 +48,10 @@ def process_test_data():
     for img in tqdm(os.listdir(TEST_DIR)):
         path = os.path.join(TEST_DIR, img)
         img_num = img.split('.')[0]
-        img = (cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMG_SIZE, IMG_SIZE))
+        img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMG_SIZE, IMG_SIZE))
         testing_data.append([np.array(img), img_num])
 
+    shuffle(testing_data)
     # np.save('test_data.npy', testing_data)
     return testing_data
 
@@ -56,12 +59,23 @@ def process_test_data():
 train_data = create_train_data()
 # train_data = np.load('train_data.npy')
 
+# tf.reset_default_graph()
+
 convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
 
 convnet = conv_2d(convnet, 32, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
 
 convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 128, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
 
 convnet = fully_connected(convnet, 1024, activation='relu')
@@ -86,4 +100,32 @@ test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 test_y = [i[1] for i in test]
 
 model.fit({'input': X}, {'targets': Y}, n_epoch=5, validation_set=({'input': test_x}, {'targets': test_y}),
-    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+          snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+test_data = process_test_data()
+# test_data = np.load('train_data.npy')
+
+fig = plt.figure()
+
+for num, data in enumerate(test_data[:12]):
+    # cat [1,0]
+    # dog [0,1]
+    img_num = data[1]
+    img_data = data[0]
+
+    y = fig.add_subplot(3, 4, num + 1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+
+    model_out = model.predict([data])[0]
+
+    if np.argmax(model_out) == 1:
+        str_label = 'Dog'
+    else:
+        str_label = 'Cat'
+
+    y.imshow(orig, cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
